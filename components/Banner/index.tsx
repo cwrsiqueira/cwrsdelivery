cdc0d2c26ab7be184840108684b0b702746aa67a
cwrsiqueira <cwrsiqueira@msn.com>
2022-07-24 13:02:44 -0300

    Implementado banner de promoções

--- components/Banner/index.tsx
+++ components/Banner/index.tsx
@@ -1,5 +1,6 @@
 import styles from "./styles.module.css"
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from "swiper";
 import 'swiper/css';
 import Image from "next/image";
 
@@ -8,12 +9,15 @@ export const Banner = () => {
         <div className={styles.container}>
             <Swiper
                 slidesPerView={1}
+                autoplay={{ delay: 2500, disableOnInteraction: false }}
+                modules={[Autoplay]}
+                loop={true}
             >
-                <SwiperSlide>
-                    <Image src="/temp/banner1.png" alt="banner1" width={380} height={190} />
+                <SwiperSlide className={styles.swiperSlide}>
+                    <Image src="/temp/banner1.png" alt="banner1" width={425} height={235} />
                 </SwiperSlide>
-                <SwiperSlide>
-                    <Image src="/temp/banner1.png" alt="banner2" width={380} height={190} />
+                <SwiperSlide className={styles.swiperSlide}>
+                    <Image src="/temp/banner2.png" alt="banner2" width={425} height={235} />
                 </SwiperSlide>
             </Swiper>
         </div>
