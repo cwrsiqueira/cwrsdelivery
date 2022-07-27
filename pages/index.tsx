0ee95276ce60bc210a93b824397b1ab98d930ef9
cwrsiqueira <cwrsiqueira@msn.com>
2022-07-27 07:39:52 -0300

    Links na primeira página

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,41 +14,22 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Bem Vindo <a href="https://nextjs.org">CWRS Delivery</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
+          Escolha a lanchonete de sua preferência
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href="https://cwrsdelivery.vercel.app/cwrsburger" className={styles.card}>
+            <h2>CWRSBurger &rarr;</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+          <a href="https://cwrsdelivery.vercel.app/cwrspizza" className={styles.card}>
+            <h2>CWRSPizza &rarr;</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate?</p>
           </a>
         </div>
       </main>
